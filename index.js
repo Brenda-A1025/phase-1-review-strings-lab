408de922cfa00fcd1a86d9ddbdc09137dc436fca
Brenda <brendanjeri2001@gmail.com>
2025-05-10 02:04:46 -0700

    initial commitment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook ' + currentUser;
+console.log(welcomeMessage);
+const welcomeMessage1 = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage1);
+const ne =currentUser.slice(4,8);
+console.log(ne);
+
+const myName= 'Njeri';
+const age = 40;
+const nickName = 'Rendy';
+const all =`my name is , ${myName}!`;
+const every= `${nickName} ${myName} ${age}!`;
+console.log(all);
+console.log(every);
+`age`.length;
